b26851973e3b62a32d5f2d1c072ad5adf921b4bc
Michele Chilanti <mchilant@micheles-mbp.rchland.ibm.com>
2019-06-19 11:30:28 -0500

    Travis test

---
+++ doGit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir test
+cd test
+git clone https://github.com/chilanti/homebrew-testmcformula.git
+git checkout -b testrelease
+cp ../testfile.txt .
+git add .
+git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcformula.git
+git push
