351e253d3cfad6c7d04cd9401c443bc2f10085d5
Michele Chilanti <mchilant@micheles-mbp.rchland.ibm.com>
2019-06-19 13:09:53 -0500

    Added remove origin

--- doGit.sh
+++ doGit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 mkdir test
 cd test
+git login 
 git clone https://github.com/chilanti/homebrew-testmcformula.git
 git checkout -b testrelease
 cp ../testfile.txt .
 git add .
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+git remote remove origin
 git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcformula.git
 git push --set-upstream origin testrelease
