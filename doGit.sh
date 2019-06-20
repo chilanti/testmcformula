eeda709a1e6b6ec5d7e6c8b8a0b3904ae1202b14
Michele Chilanti <mchilant@micheles-mbp.rchland.ibm.com>
2019-06-20 17:01:37 -0500

    Changed sequence added config

--- doGit.sh
+++ doGit.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 env
-mkdir /mctest
-cd /mctest
-git remote remove origin
+#git remote remove origin
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcformula.git
-git clone https://github.com/chilanti/homebrew-testmcformula.git
+#git clone https://github.com/chilanti/homebrew-testmcformula.git
 git checkout -b test${TRAVIS_BUILD_NUMBER}
-cp ../testfile.txt .
+#cp ../testfile.txt .
 git add testfile.txt
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcformula.git
 git push --set-upstream origin test${TRAVIS_BUILD_NUMBER}
