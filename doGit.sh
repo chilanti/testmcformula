c5fc76c420bd09216770fa2eaaa5c43dd5f1606b
Michele Chilanti <mchilant@micheles-mbp.rchland.ibm.com>
2019-06-20 17:11:29 -0500

    echo something into testfile

--- doGit.sh
+++ doGit.sh
@@ -9,7 +9,7 @@ git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcfor
 #git clone https://github.com/chilanti/homebrew-testmcformula.git
 git checkout -b test${TRAVIS_BUILD_NUMBER}
 #cp ../testfile.txt .
-touch testfile.txt
+echo something >> testfile.txt
 git add testfile.txt
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 git push --set-upstream origin test${TRAVIS_BUILD_NUMBER}
