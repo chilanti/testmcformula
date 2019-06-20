e6a9cbb2e2551958ec68850a349e57e4343d7e71
Michele Chilanti <mchilant@micheles-mbp.rchland.ibm.com>
2019-06-20 16:49:47 -0500

    Limited push to one file

--- doGit.sh
+++ doGit.sh
@@ -8,6 +8,6 @@ git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcfor
 git clone https://github.com/chilanti/homebrew-testmcformula.git
 git checkout -b test${TRAVIS_BUILD_NUMBER}
 cp ../testfile.txt .
-git add .
+git add testfile.txt
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 git push --set-upstream origin test${TRAVIS_BUILD_NUMBER}
