7e2c9d1a1918589a7f8a03a08680241d8273585a
Michele Chilanti <mchilant@micheles-mbp.rchland.ibm.com>
2019-06-20 17:19:23 -0500

    added a new origin

--- doGit.sh
+++ doGit.sh
@@ -4,12 +4,13 @@ env
 #git remote remove origin
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git remote remove origin
-git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcformula.git
+
+
 #git clone https://github.com/chilanti/homebrew-testmcformula.git
 git checkout -b test${TRAVIS_BUILD_NUMBER}
 #cp ../testfile.txt .
 echo something >> testfile.txt
 git add testfile.txt
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
-git push --set-upstream origin test${TRAVIS_BUILD_NUMBER}
+git remote add origin-2 https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcformula.git
+git push --set-upstream origin-2 test${TRAVIS_BUILD_NUMBER}
