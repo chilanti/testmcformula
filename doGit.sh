3bf025fb6b9fb776bb38ffa7590e60c7a0e2e545
Michele Chilanti <mchilant@micheles-mbp.rchland.ibm.com>
2019-06-20 16:43:09 -0500

    Changed the sequence of git operations

--- doGit.sh
+++ doGit.sh
@@ -3,11 +3,11 @@ set -e
 env
 mkdir ./mctest
 cd mctest
+git remote remove origin
+git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcformula.git
 git clone https://github.com/chilanti/homebrew-testmcformula.git
 git checkout -b test${TRAVIS_BUILD_NUMBER}
 cp ../testfile.txt .
 git add .
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
-git remote remove origin
-git remote add origin https://${GH_TOKEN}@github.com/chilanti/homebrew-testmcformula.git
 git push --set-upstream origin test${TRAVIS_BUILD_NUMBER}
